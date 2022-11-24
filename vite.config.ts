cb2dcd93da354a6e85c51c08f31e8e5736e4d821
sabasayer <salih.b.sayer@gmail.com>
2022-11-24 21:40:01 +0300

    fix: module output

--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,26 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 const resolvePath = (str: string) => path.resolve(__dirname, str);
 
 export default defineConfig({
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: resolvePath("src/index.ts"),
-      name: "index",
-      fileName: (format) => `index.${format}.js`,
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: resolvePath("src/index.ts"),
+            name: "index",
+            fileName: "index",
+        },
     },
-  },
-  plugins: [
-    peerDepsExternal(),
-    typescript({
-      rootDir: resolvePath("src"),
-      declaration: true,
-      declarationDir: resolvePath("dist"),
-      exclude: [
-        resolvePath("node_modules/**"),
-        resolvePath("src/**/*.spec.ts"),
-        resolvePath("src/__tests__/*.ts"),
-        resolvePath("src/__mocks__/*.ts"),
-      ],
-    }),
-  ],
+    plugins: [
+        peerDepsExternal(),
+        typescript({
+            rootDir: resolvePath("src"),
+            declaration: true,
+            declarationDir: resolvePath("dist"),
+            exclude: [
+                resolvePath("node_modules/**"),
+                resolvePath("src/**/*.spec.ts"),
+                resolvePath("src/__tests__/*.ts"),
+                resolvePath("src/__mocks__/*.ts"),
+            ],
+        }),
+    ],
 });
