3ae45b5743568bda9f7eccaa7b897f067f8220a1
sabasayer <salih.b.sayer@gmail.com>
2022-12-30 20:38:17 +0300

    chore: export types

--- src/module/index.ts
+++ src/module/index.ts
@@ -2,5 +2,7 @@ import type { ICoreModule } from "./core-module.interface";
 import { CoreModule } from "./core-module";
 
 export * from "./dependency-maps"
+export type { DependencyResolveOptions } from "./resolve-options";
+export type { ResolveType } from "./resolve-type"
 export type { ICoreModule };
 export { CoreModule };
