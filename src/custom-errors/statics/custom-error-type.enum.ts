199969c0e6f91d4053197a3c6df4f7d8ee685644
sabasayer <salih.b.sayer@gmail.com>
2023-01-10 22:17:12 +0300

    refactor: custom error types

--- src/custom-errors/statics/custom-error-type.enum.ts
+++ src/custom-errors/statics/custom-error-type.enum.ts
@@ -1,9 +1,14 @@
 export enum EnumCustomErrorType {
     Server = "Server",
     AbortedRequest = "AbortedRequest",
+    FormValidation = "FormValidation",
     RequestValidation = "RequestValidation",
     ResponseValidation = "ResponseValidation",
     Construction = "Construction",
     BusinessLogic = "BusinessLogic",
     Permission = "Permission",
+    Navigation = "Navigation",
+    ActionPermission = "Permission",
+    NavigationPermission = "Permission",
+    Application = "Application",
 }
