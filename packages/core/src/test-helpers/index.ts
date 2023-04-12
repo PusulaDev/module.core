ec05dd265a855b8f0e33647ded3549a27701a01d
sabasayer <salih.b.sayer@gmail.com>
2023-04-12 13:19:32 +0300

    refactor(test): use key as class or string

--- packages/core/src/test-helpers/index.ts
+++ packages/core/src/test-helpers/index.ts
@@ -13,6 +13,8 @@ export const createMock = (
     module: ICoreModule,
     options: { cacheKey?: string; mockClientOnClear?: boolean; onlyClearInstances?: boolean } = {}
 ) => {
+    const getName = (key: string | IClassConstructor) => (typeof key === "string" ? key : key.name);
+
     const { cacheKey = "SessionStorageCache", mockClientOnClear = true, onlyClearInstances = true } = options;
 
     const mockHttpClient = () => module.registerHttpClient(FetchHTTPClient, { baseUrl: "localhost:9999" });
@@ -20,22 +22,28 @@ export const createMock = (
     const mockSessionCache = () => module.register(MemoryCache, { key: cacheKey });
     const getCache = () => module.resolve<MemoryCache>(cacheKey);
 
-    const mockProvider = <T extends IProvider>(provider: IProviderConstructor<T>, key: string) =>
-        module.registerProvider(provider, { key });
+    const mockProvider = <T extends IProvider>(
+        provider: IProviderConstructor<T>,
+        key: string | IClassConstructor
+    ) => module.registerProvider(provider, { key: getName(key) });
 
-    const mockProviderInstance = <T extends IProvider>(instance: T, key: string) =>
-        module.registerProviderInstance(instance, key);
+    const mockProviderInstance = <T extends IProvider>(instance: T, key: string | IClassConstructor) =>
+        module.registerProviderInstance(instance, getName(key));
 
     const getProvider = <T extends IProvider>(key: string) => module.resolveProvider<T>(key);
 
-    const mock = <T>(constructor: IClassConstructor<T>, key: string, dependencies?: DependencyType[]) =>
+    const mock = <T>(
+        constructor: IClassConstructor<T>,
+        key: string | IClassConstructor,
+        dependencies?: DependencyType[]
+    ) =>
         module.register(constructor, {
             dependencies,
-            key,
+            key: getName(key),
         });
 
-    const mockInstance = <T extends object>(instance: T, key: string) => {
-        module.registerInstance(instance, key);
+    const mockInstance = <T extends object>(instance: T, key: string | IClassConstructor) => {
+        module.registerInstance(instance, getName(key));
     };
 
     const get = <T extends IClassConstructor>(key: string) => module.resolve<T>(key);
